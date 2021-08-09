d452294688540a2bf824840b215aafc994762760
Ritik Agrawal <67484893+RitikAg2710@users.noreply.github.com>
2021-08-09 22:17:32 +0530

    Add files via upload

---
+++ PatternPrograms/Sixth.java
@@ -0,0 +1,20 @@
+package com.company.PatternPrograms;
+
+public class Sixth {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                if(i>=2 && k>1){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
